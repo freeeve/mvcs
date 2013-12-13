c19d2015e0b55c25b4c3d1a8f34436013eca2b24
Nigel Small <nigel@nigelsmall.name>
2013-12-13 22:28:48 +0000

    few tweaks

--- python/py2neo/example.py
+++ python/py2neo/example.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from py2neo import neo4j
 
 def hello_cypher():
-  graph_db = neo4j.GraphDatabaseService()
-  query_str = """match (n:User)-[:FOLLOWS]->(m:User)
-          where n.screenName = {screenName}
-          return m.screenName as friend
-          order by friend"""
-  query = neo4j.CypherQuery(graph_db, query_str)
-  results = query.execute(screenName="wefreema")
-  arr = []
-  for r in results.data:
-    arr += r
-  return arr
+    graph_db = neo4j.GraphDatabaseService()
+    query_str = """\
+    match (n:User)-[:FOLLOWS]->(m:User)
+    where n.screenName = {screenName}
+    return m.screenName as friend
+    order by friend
+    """
+    query = neo4j.CypherQuery(graph_db, query_str)
+    results = query.execute(screenName="wefreema")
+    return [result.friend for result in results]
+
