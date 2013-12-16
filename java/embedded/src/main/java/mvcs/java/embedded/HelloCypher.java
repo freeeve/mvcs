378389539601a8228d94772c5cd617a4a78cc516
sylvainroussy <sylvain.roussy@free.fr>
2013-12-16 15:01:21 +0100

    Adding ImpermanentDB + refactoring

--- java/embedded/src/main/java/mvcs/java/embedded/HelloCypher.java
+++ java/embedded/src/main/java/mvcs/java/embedded/HelloCypher.java
@@ -1,35 +1,31 @@
 package mvcs.java.embedded;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import mvcs.java.commons.AbstractHelloCypher;
+import mvcs.java.commons.HelloCypherUtils;
+
 import org.neo4j.cypher.CypherException;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
-import org.neo4j.graphdb.GraphDatabaseService;
+
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
-public class HelloCypher {
+public class HelloCypher extends AbstractHelloCypher{
 
-	private static final String DATA_CQL_RESOURCE = "data.cql";
-	private static final String PARAM_VALUE_WEFREEMA = "wefreema";
-	private static final String PARAMETER_NAME = "parameter_name";
-	private static final String COLUMN_AS_FRIEND = "friend";
-	private static final String CYPHER_HELLO_QUERY = "match (n:User)-[:FOLLOWS]->(m:User) where n.screenName = {parameter_name} return m.screenName as friend order by friend";
+	
+	
 	private static final String TEST_DB = "./test.db";
-	private GraphDatabaseService db ;
+	
 	private ExecutionEngine engine;
 
 	public HelloCypher ()
 	{
-		this.buildDB();
-		this.loadGraph();
+		super();
+		
 	}
 	
 	public List<String> hello () throws CypherException
@@ -47,44 +43,15 @@ public class HelloCypher {
 		return results;
 	}
 	
-	private void buildDB ()
+	protected void buildDB ()
 	{
 		db = new GraphDatabaseFactory().newEmbeddedDatabase( TEST_DB );
 		Runtime.getRuntime().addShutdownHook(new Thread (){public void run (){db.shutdown();}});
-		engine = new ExecutionEngine( db );		
+		engine = new ExecutionEngine( db );	
+		HelloCypherUtils.loadGraph(engine);
 	}
 	
-	private void loadGraph ()
-	{
-		final InputStream is = this.getClass().getClassLoader().getResourceAsStream(DATA_CQL_RESOURCE);
-		if (is == null)
-		{
-			System.out.println(" Resource not found ");
-		}
-		else
-		{
-			final InputStreamReader isr = new InputStreamReader(is);			
-			final BufferedReader br = new BufferedReader(isr);
-			
-			try
-			{
-				final StringBuffer query = new StringBuffer ();
-				String line = br.readLine();			
-				while (line != null)
-				{					
-					query.append(line);
-					line = br.readLine();
-				}
-				
-				br.close();
-				engine.execute(query.toString());
-			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-	}
+	
 	
 	
 }
--- java/embedded/src/test/java/mvcs/java/embedded/test/HelloCypherTest.java
+++ java/embedded/src/test/java/mvcs/java/embedded/test/HelloCypherTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 import mvcs.java.embedded.HelloCypher;
+import mvcs.java.embedded.HelloCypherImpermanent;
 
 import org.junit.Test;
 
@@ -16,6 +17,24 @@ public class HelloCypherTest {
 	public void helloCypherTest ()
 	{
 		final HelloCypher hc = new HelloCypher ();
+		final List<String> results = hc.hello();
+		String JnBrymn = null; 
+		String technige= null;
+		if (results.size() > 0)
+		{
+			JnBrymn = results.get(0);
+			technige =  results.get(1);
+		}
+		
+		assertEquals("JnBrymn", JnBrymn);
+		assertEquals("technige", technige);
+		
+	}
+	
+	@Test
+	public void helloImpermanentCypherTest ()
+	{
+		final HelloCypherImpermanent hc = new HelloCypherImpermanent();
 		final List<String> results = hc.hello();
 		String JnBrymn = null;
 		String technige= null;
